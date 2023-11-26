4596b3d6d784b115a66971a1354fbb5ad844fef6
Alex R <Optmsprm323@gmail.com>
2023-11-26 16:42:41 -0500

    Added the second part of the main function.

--- InheritanceProject/InheritanceProject.cpp
+++ InheritanceProject/InheritanceProject.cpp
@@ -40,5 +40,46 @@ int main()
     }
     userVehicle.setYearBuilt(tempInt);
     userVehicle.displayInfo();
+    cout << endl << endl;
+
+    cout << "Car:" << endl;
+    cout << "Enter the manufacturer: ";
+    cin.ignore();
+    getline(cin, tempStr);
+    userCar.setManufacturer(tempStr);
+    while (true)
+    {
+        cout << "Enter the year built: ";
+        cin >> tempInt;
+        if (!cin.good() || to_string(tempInt).length() != 4)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid Choice" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    userCar.setYearBuilt(tempInt);
+    while (true)
+    {
+        cout << "Enter the number of doors: ";
+        cin >> tempInt;
+        if (!cin.good() || tempInt < 1)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid Choice" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    userCar.setNumDoors(tempInt);
+    userCar.displayInfo();
+    cout << endl << endl;
 }
 
