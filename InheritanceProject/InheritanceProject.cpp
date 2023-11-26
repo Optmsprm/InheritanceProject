c052229d5a9ce125940a879f62e93f2964af76d0
Alex R <Optmsprm323@gmail.com>
2023-11-26 16:16:38 -0500

    Added the first part of the main function.

--- InheritanceProject/InheritanceProject.cpp
+++ InheritanceProject/InheritanceProject.cpp
@@ -2,13 +2,43 @@
 // CIS 1202 501
 // 11/26/2023
 
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    
+    Vehicle userVehicle;
+    Car userCar;
+    Truck userTruck;
+    string tempStr;
+    int tempInt;
+
+    cout << "Vehicle Program" << endl << endl;
+
+    cout << "Vehicle: " << endl;
+    cout << "Enter the manufacturer: ";
+    getline(cin, tempStr);
+    userVehicle.setManufacturer(tempStr);
+    while(true)
+    {
+        cout << "Enter the year built: ";
+        cin >> tempInt;
+        if (!cin.good() || to_string(tempInt).length() != 4)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid Choice" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    userVehicle.setYearBuilt(tempInt);
+    userVehicle.displayInfo();
 }
 
